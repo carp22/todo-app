fe71803d7da63d1000bc977ca406e087a1deec3e
carp22 <carlosrp2207@gmail.com>
2023-01-12 14:27:58 -0400

    Count fixed
    Items left counter fixed!
    Alert message created! So users cant entered a blank space todo

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,33 @@ import { useState, useEffect } from "react";
 import { nanoid } from "nanoid";
 
 function App() {
+  
   const [darkMode, setDarkMode] = useState("light");
-
-
   const [todoList, setTodoList] = useState([]);
-  const [newEntry, setNewEntry] = useState([]);
+  const [newEntry, setNewEntry] = useState("");
   const [whatToDisplay, setWhatToDisplay] = useState("all");
+
+  const [itemsLeft, setItemsLeft] = useState(0)
+
+  const createNewTodo = function () {
+    if (newEntry == "") {
+      return alert("A blank todo is not allowed")
+    }
+    const newTodo = {
+      id: nanoid(),
+      description: newEntry,
+      isSelected: false
+    };
+    setNewEntry("");
+    setTodoList(oldState => [newTodo, ...oldState]);
+    setItemsLeft(oldState => oldState + 1)
+  };
 
 
   useEffect(() => {
     document.body.className = darkMode;
   }, [darkMode]);
+
 
   const newTodoItem = function (event) {
     const { value } = event.target;
@@ -28,24 +44,27 @@ function App() {
     }
   };
 
-  const createNewTodo = function () {
-    const newTodo = {
-      id: nanoid(),
-      description: newEntry,
-      isSelected: false
-    };
-    setNewEntry("");
-    setTodoList(oldState => [newTodo, ...oldState]);
-  };
-
   const todoStatus = function (id) {
+
+    todoList.map(item=> {
+      if (id === item.id) {
+        if (event.target.checked) {
+          setItemsLeft(oldState => oldState - 1)
+        } else {
+          setItemsLeft(oldState => oldState + 1)
+        }
+      } 
+    })
+
     setTodoList(list => list.map(item => {
-      return id === item.id ? {
+      return id === item.id ? 
+      {
         ...item,
         isSelected: !item.isSelected
       } : item;
-    }));
-  };
+    }))
+  }
+
   const deleteTodo = function () {
     for (let i = 0; i < todoList.length; i++) {
       const item = todoList[i];
@@ -70,10 +89,11 @@ function App() {
     if (whatToDisplay === "completed") return item.isSelected;
     if (whatToDisplay === "active") return !item.isSelected;
   }).map(item => {
+
     return (
       <div className={`todo-element ${darkMode === "light" ? "item-lgth" : "item-drk"}`} key={item.id} >
         <>
-          <input type="radio" className="todo-radio" id={`todo-${item.id}`} onClick={() => todoStatus(item.id)} />
+          <input type="checkbox" className="todo-radio" id={`todo-${item.id}`} onClick={() =>  todoStatus(item.id) }/>
           <h3 className={`todo-label ${item.isSelected && "status"}`}>{item.description}</h3>
         </>
       </div>
@@ -96,7 +116,7 @@ function App() {
         {todoItems}
         <div className={`todo-settings ${darkMode === "light" ? "item-lgth" : "item-drk"}`}>
 
-          <h4 className={`${darkMode === "light" ? "setting-lght" : "setting-drk"}`} onClick={() => console.log(todoList)}>Items left</h4>
+          <h4 className={`${darkMode === "light" ? "setting-lght" : "setting-drk"}`} onClick={() => console.log(todoList)}> {itemsLeft} Items left</h4>
 
           <div className="todo-status">
             <h4 className={`${darkMode === "light" ? "setting-lght" : "setting-drk"}`} onClick={allTodos}>All</h4>
--- src/components/TodoElements.jsx
+++ src/components/TodoElements.jsx
@@ -5,18 +5,16 @@ import moon from "../../public/images/icon-moon.svg"
 function TodoElements({darkMode, toggleMode, createNewTodo, newEntry, newTodoItem}) {
   return (
     <>
-    <div className= "todo-body">
-      <h2 className='todo-title'>TODO</h2>
-      <img src={darkMode ? sun : moon} alt="mode" 
-          className='mode-icon'
-          onClick={toggleMode}/>
-      </div>
-      <div className="newtodo-container">
-        <input type="checkbox" className='radio' onClick={createNewTodo} />
-        <input type="text" className={`newtodo-input ${darkMode === "light" ? "item-lgth" : "item-drk"}`} placeholder='Create a new todo...' value={newEntry} onChange={newTodoItem}/>
-      </div>
-
-      
+      <div className= {`todo-body ${darkMode === "light" ? "lght-background" : "drk-background"}`} >
+        <h2 className='todo-title'>TODO</h2>
+        <img src={darkMode === "light" ? moon : sun} alt="mode" 
+            className='mode-icon'
+            onClick={toggleMode}/>
+        </div>
+        <div className="newtodo-container">
+          <input type="button" className='buttonNewTodo' onClick={createNewTodo} />
+          <input type="text" className={`newtodo-input ${darkMode === "light" ? "item-lgth" : "item-drk"}`} placeholder='Create a new todo...' value={newEntry} onChange={newTodoItem}/>
+        </div>
     </>
   )
 }
